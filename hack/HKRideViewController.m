e0e795023b797c1c7dda29b81cda744ab4de89c7
Boris <zhouboli@gmail.com>
2015-07-23 10:16:24 +0800

    fixed rideview

--- hack/HKRideViewController.m
+++ hack/HKRideViewController.m
@@ -68,16 +68,7 @@
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    NSString *driverAvatarUrl = _request.driver.picture_url;
-    if (driverAvatarUrl) {
-        [_driverAvatarView sd_setImageWithURL:[NSURL URLWithString:driverAvatarUrl] placeholderImage:[UIImage imageNamed:@"hk_driver_avatar"]];
-    }
-    NSString *vehicleImageUrl = _request.vehicle.picture_url;
-    if (vehicleImageUrl) {
-        [_vehicleView sd_setImageWithURL:[NSURL URLWithString:vehicleImageUrl] placeholderImage:[UIImage imageNamed:@"hk_vehicle_avatar"]];
-    }
-    _driverInfoLabel.text = [NSString stringWithFormat:@"司机信息：\n请求状态：%@，名称：%@，电话：%@，评分：%.1f，%ld后可接驾", _request.status, _request.driver.name, _request.driver.phone_number, _request.driver.rating, _request.eta];
-    _vehicleInfoLabel.text = [NSString stringWithFormat:@"车辆信息：\n品牌：%@，型号：%@，车牌号：%@", _request.vehicle.make, _request.vehicle.model, _request.vehicle.license_plate];
+    [self refreshStatus];
 }
 
 #pragma mark - UIButtons
@@ -92,6 +83,27 @@
         if ([button.titleLabel.text isEqualToString:@"取消请求"]) {
             [self cancelRequest];
         }
+    }
+}
+
+#pragma mark - Helpers
+
+-(void)refreshStatus
+{
+    NSString *driverAvatarUrl = _request.driver.picture_url;
+    if (driverAvatarUrl) {
+        [_driverAvatarView sd_setImageWithURL:[NSURL URLWithString:driverAvatarUrl] placeholderImage:[UIImage imageNamed:@"hk_driver_avatar"]];
+    }
+    NSString *vehicleImageUrl = _request.vehicle.picture_url;
+    if (vehicleImageUrl) {
+        [_vehicleView sd_setImageWithURL:[NSURL URLWithString:vehicleImageUrl] placeholderImage:[UIImage imageNamed:@"hk_vehicle_avatar"]];
+    }
+    if ([_request.status isEqualToString:@"accepted"]) {
+        _driverInfoLabel.text = [NSString stringWithFormat:@"司机信息：\n请求状态：%@，名称：%@，电话：%@，评分：%.1f，%ld后可接驾", _request.status, _request.driver.name, _request.driver.phone_number, _request.driver.rating, _request.eta];
+        _vehicleInfoLabel.text = [NSString stringWithFormat:@"车辆信息：\n品牌：%@，型号：%@，车牌号：%@", _request.vehicle.make, _request.vehicle.model, _request.vehicle.license_plate];
+    }
+    if ([_request.status isEqualToString:@"processing"]) {
+        _driverInfoLabel.text = [NSString stringWithFormat:@"司机信息：\n请求状态：%@", _request.status];
     }
 }
 
@@ -128,6 +140,7 @@
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             [[UberKit sharedInstance] getDetailsForRequestId:_request.request_id withCompletionHandler:^(UberRequest *requestResult, UberSurgeErrorResponse *surgeErrorResponse, NSURLResponse *response, NSError *error) {
                 dispatch_async(dispatch_get_main_queue(), ^{
+                    [self refreshStatus];
                     [[[UIAlertView alloc] initWithTitle:@"Uber Response" message:[NSString stringWithFormat:@"UberResponse:\nrequest_id: %@\nstatus: %@\neta: %ld\nsurge_multiplier: %f\nvehicle:\nmake: %@\nmodel: %@\nlicense_plate: %@\ndriver:\nphone_number: %@\nname: %@\nrating: %f\nlocation:\nlat: %f lon: %f bearing: %ld\nresponse: %@\nerror: %@", requestResult.request_id, requestResult.status, requestResult.eta, requestResult.surge_multiplier, requestResult.vehicle.make, requestResult.vehicle.model, requestResult.vehicle.license_plate, requestResult.driver.phone_number, requestResult.driver.name, requestResult.driver.rating, requestResult.location.latitude, requestResult.location.longitude, requestResult.location.bearing, response, error] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
                 });
             }];
